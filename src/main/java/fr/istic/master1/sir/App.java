daa905be3593cea044cc4a72afd926bd02ca9c0c
vinyard <antoineleval@gmail.com>
2018-09-12 11:06:22 +0200

    init

---
+++ src/main/java/fr/istic/master1/sir/App.java
@@ -0,0 +1,18 @@
+package fr.istic.master1.sir;
+
+/**
+ * Hello world!
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
+	private void methodeInutile() {
+		System.out.println("Hello World!");
+	}
+	
+	private void methodeInutile2() {
+		System.out.println("Hello World!");
+	}
+}
